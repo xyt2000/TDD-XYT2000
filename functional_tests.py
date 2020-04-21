d33c28a326b5d683ef74e737a07cd4d32a4b82d7
1026310947 <1026310957@qq.com>
2020-04-21 11:17:00 +0800

    some changes in ft about check

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,10 @@ class NewVisitorTest(unittest.TestCase):#(1)
 	def tearDown(self):#(3)
 		self.browser.quit()
 		
+	def check_for_row_in_list_table(self,row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
 	def test_can_start_a_list_and_retrieve_it_later(self):#(2)
 		#Edith has heard about a cool new online to-do app
 		#she goes to check out its homepages
@@ -25,7 +29,7 @@ class NewVisitorTest(unittest.TestCase):#(1)
 		)
 		#she types "Buy peocock feathers" into a text box(Ediths hobby
 		#is tying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys('Use peacock feathers to make a fly')
 		#when she hits enter,the page updates,and now the page lists
 		#"1":Buy peocock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
@@ -36,8 +40,8 @@ class NewVisitorTest(unittest.TestCase):#(1)
 		#	any(row.text == '1:Buy peacock feathers' for row in rows),
 		#	f"New to-do item did not appear in table. Contents were:\n{table.text}"
 		#)
-		self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-		self.assertIn('2:Use peacock feathers to make a fly',[row.text for row in rows])
+		self.check_for_row_in_list_table('1:Buy peacock feathers')
+		self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
 		#There is still a text box inviting her to add another item.She
 		#enters "use peocock feathers to make a fly"(Edith is very methodical)
 		self.fail('finish the test')
