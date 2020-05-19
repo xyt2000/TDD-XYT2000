80db8904b8be795b704316b98a7a456641b918a4
1026310947 <1026310957@qq.com>
2020-05-19 11:05:35 +0800

    add new html view

--- lists/views.py
+++ lists/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render,redirect
 from lists.models import Item
 from django.http import HttpResponse
 # Create your views decode
+def view_list(request):
+	items = Item.objects.all()
+	return render(request,'list.html',{'items':items})
 def home_page(request):
 	if request.method =='POST':
 		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')
-	items = Item.objects.all()
-	return render(request,'home.html',{'items':items})
+		return redirect('/lists/the-only-list-in-the-world/')
+	return render(request,'home.html')
